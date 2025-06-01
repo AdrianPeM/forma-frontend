f9f10927fd3c0661c661c469d12a3b7b744de90c
Adrian Perez <adrianperezm.00@gmail.com>
2025-06-01 14:53:19 -0600

    feat: add ESLint

--- src/genericTypes.ts
+++ src/genericTypes.ts
@@ -1,14 +1,14 @@
 import type { CSSProperties } from "react"
 
 export type CSSPropertiesWithVariables = CSSProperties & {
-	[key: `--${string}`]: string
+  [key: `--${string}`]: string
 }
 
 export type GenericPrimitiveValue = null | undefined | number | string | boolean | Date
 export type GenericValue = GenericPrimitiveValue | GenericObject | Array<GenericValue>
 
 export type GenericObject = {
-	[key: string]: GenericValue
+  [key: string]: GenericValue
 }
 
 export type Unpacked<T> = T extends (infer U)[] ? U : never
@@ -18,9 +18,9 @@ export type Nullable<T> = { [K in keyof T]: T[K] | null }
 export type UnpackKeyOfArray<T, K extends keyof T> = T[K]
 
 export type UnderscoredString<T extends string, C extends string = ""> = T extends `${infer F} ${infer R}`
-	? UnderscoredString<R, C extends "" ? F : `${C}_${F}`>
-	: C extends ""
-		? T
-		: `${C}_${T}`
+  ? UnderscoredString<R, C extends "" ? F : `${C}_${F}`>
+  : C extends ""
+    ? T
+    : `${C}_${T}`
 
 export type UppercaseUnderscoredString<K extends string> = Uppercase<UnderscoredString<K>>
